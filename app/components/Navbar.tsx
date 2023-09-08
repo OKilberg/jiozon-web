d6387297bce27e8d1f61c6d05b76389040637353
OKilberg <oscar.oravakilberg@gmail.com>
2023-09-08 11:47:39 +0200

    testing this bottom navbar for mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <nav className='w-full text-green-text flex justify-center gap-8 md:gap-12 py-3 font-jomhuria text-3xl'>
-        <div>Home</div>
+    <nav className='bg-white fixed bottom-0  md:static z-10 w-full text-green-text flex justify-center gap-8 md:gap-12 py-3 font-jomhuria text-3xl'>
+        <div className='align-middle'>Home</div>
         <div>Media</div>
         <div>Producer</div>
         <div>Biography</div>
