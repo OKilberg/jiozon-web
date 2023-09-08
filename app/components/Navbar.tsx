6ff6d2fa31f866cc753c4da920ec0472e8135ac1
OKilberg <oscar.oravakilberg@gmail.com>
2023-09-08 12:18:14 +0200

    nav now uses columns instead of flex

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 type Props = {}
 
-export default function Navbar({}: Props) {
+export default function Navbar({ }: Props) {
   return (
-    <nav className='bg-white fixed bottom-0  md:static z-10 w-full text-green-text flex justify-center gap-8 md:gap-12 py-3 font-jomhuria text-3xl'>
-        <div className='align-middle'>Home</div>
-        <div>Media</div>
-        <div>Producer</div>
-        <div>Biography</div>
+    <nav className='bg-white fixed bottom-0  md:static z-10 w-full text-green-text grid grid-cols-4 md:grid-cols-8 md:gap-12 py-3 font-jomhuria text-3xl'>
+      <div className='grid grid-cols-4 col-span-4 md:col-start-3'>
+        <div className='flex justify-center'>Home</div>
+        <div className='flex justify-center'>Media</div>
+        <div className='flex justify-center'>Producer</div>
+        <div className='flex justify-center'>Biography</div>
+      </div>
     </nav>
   )
 }
